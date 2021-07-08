eff79db170537e92f04d565f9aa30d8aaf0b7e13
Michealin White <mich.white@hotmail.com>
2021-07-08 16:19:05 +0100

    first commit

---
+++ app/src/main/java/com/example/localjobs/WorkersFragment.kt
@@ -0,0 +1,27 @@
+package com.example.localjobs
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.layout_workers.*
+private var ctx: Context? = null
+private var self: View? = null
+
+class WorkersFragment : Fragment() {
+override fun onCreateView(inflater: LayoutInflater,
+                          container: ViewGroup?,
+                          savedInstanceState: Bundle?): View? {
+    ctx = container?.context
+    self = LayoutInflater.from(ctx).inflate(R.layout.layout_workers, container, false)
+    val bDaButton = self?.findViewById<androidx.cardview.widget.CardView>(R.id.Barber)
+    bDaButton?.setOnClickListener {
+        Toast.makeText(ctx, "button works!", Toast.LENGTH_SHORT).show()
+    }
+    return self
+}
+}
